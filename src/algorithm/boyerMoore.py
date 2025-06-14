a000fe94f9244a758998caf898c593b509844721
BrianHadianSTEI23 <bri.hadian@gmail.com>
2025-06-15 01:03:53 +0700

    fix : bug on boyer moore logic

--- src/algorithm/boyerMoore.py
+++ src/algorithm/boyerMoore.py
@@ -1,6 +1,10 @@
-
+import re
 
 # specification: get the bad match table, then do pattern matching from the end
+
+def clean_alnum_only(s: str) -> str:
+    return re.sub(r'[^a-zA-Z0-9 ]', '', s.lower())
+
 
 def getBadMatchTable(pattern : str) -> dict[str, int]:
 
@@ -9,11 +13,14 @@ def getBadMatchTable(pattern : str) -> dict[str, int]:
     2. put into bad match table
     3. run through the pattern for its index'''
 
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "a", "b",
+                "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ":", "/", "\\", "'", "\"", "?",
+                ">", "<", "]", "[", "{", "}", "|", "!", "@", "#"," $", "%", "^", "&", "*", "(", ")", "+", "_", "-", "=", "`", "~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                "\n", ",", "."]
     badMatchTable : dict[str, int]= {}
     # fill bad match table with all alphabet
     for i in range(len(alphabet)):
-        badMatchTable.update({alphabet[i].lower() : -1})
+        badMatchTable.update({alphabet[i] : -1})
 
     for j in range(len(pattern)) :
         badMatchTable.update({ pattern[j] : j})
@@ -23,6 +30,7 @@ def getBadMatchTable(pattern : str) -> dict[str, int]:
 def boyerMooreMatch(text : str, keywords : list[str]) -> dict[str, int]:
 
     result = {}
+    clean_text = clean_alnum_only(text)
 
     for keyword in keywords:
         if len(text) >= len(keyword) :
@@ -30,22 +38,22 @@ def boyerMooreMatch(text : str, keywords : list[str]) -> dict[str, int]:
             iterForPattern = len(keyword) - 1 
             # determine its bad match table
             badMatchTable = getBadMatchTable(keyword)
-            while (iterForText < (len(text) - len(keyword))):
+            while (iterForText < len(clean_text)):
 
-                if (text[iterForText] == keyword[iterForPattern] and (iterForPattern == 0)):
+                if (clean_text[iterForText] == keyword[iterForPattern] and (iterForPattern == 0)):
                     if (keyword in result.keys()):
                         result.update({keyword : result[keyword] + 1})
                     else : # hasn't been there
                         result.update({ keyword : 1})
                     iterForPattern = len(keyword) - 1 
                     iterForText += len(keyword)
-                elif (text[iterForText] == keyword[iterForPattern]):
+                elif (clean_text[iterForText] == keyword[iterForPattern]):
                     iterForText -= 1
                     iterForPattern -= 1
                 else : # not the same
-                    if (badMatchTable[text[iterForText]] > -1): 
-                        if (badMatchTable[text[iterForText]] < iterForPattern): # occurs again later
-                            iterForText += max(1, iterForPattern - badMatchTable[text[iterForText]])
+                    if (badMatchTable[clean_text[iterForText]] > -1): 
+                        if (badMatchTable[clean_text[iterForText]] <= iterForPattern): # occurs again later
+                            iterForText += max(1, iterForPattern - badMatchTable[clean_text[iterForText]])
                         else : # not occurs again later
                             iterForText += len(keyword) - iterForPattern
                     else : # not in bad match table
@@ -53,3 +61,7 @@ def boyerMooreMatch(text : str, keywords : list[str]) -> dict[str, int]:
                     iterForPattern = len(keyword) - 1 # go back to the last
 
     return result
+
+
+if __name__ == "__main__":
+    print(boyerMooreMatch("hayo apa hayo", ["ayo", "a"]))
